34c3a98d737035aea3af8b74e599c8e09ea71d05
oneflyingfish <yt931379339@gmail.com>
2020-11-24 05:13:46 -0800

    create users test db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+99.times do |n|
+    name  = Faker::Name.name
+    num = "%03d"%(n+1)
+    user_number="#{rand(2019..2022)}#{rand(1..2)}#{rand(50149..50152)}#{rand(55..60)}#{num}"
+    email = "test_#{n+1}@eamil.com"
+    password = "password"
+    User.create!(name: name ,user_number: user_number, email: email, password: password, password_confirmation: password)
+end
