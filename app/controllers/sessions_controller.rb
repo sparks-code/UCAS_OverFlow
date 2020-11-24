4d00f35e892373016d920a9695ea44a417db9597
oneflyingfish <yt931379339@gmail.com>
2020-11-23 22:22:49 -0800

    Login Session Controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,25 +1,24 @@
 class SessionsController < ApplicationController
   def new
+    # no code
   end
 
   def create
-    # 请在下面完成要求的功能
-    #*********begin*********#
     user = User.find_by(email: params[:session][:email])
-      if @user.nil?
+      if user.nil?
         # 用户不存在
-        flash.now[:danger] = "登录失败，用户名/密码错误！"
+        flash.now[:danger] = "登录失败，用户不存在！"
         render "new"
       else
         unless user.authenticate(params[:session][:password])
-          flash.now[:danger] = "登录失败，用户名/密码错误！"
+          flash.now[:danger] = "登录失败，密码错误！"
           render "new"
         else
           flash.now[:success] = "登录成功！"  
-          render "new" 
+
+          # 登录成功，重定向到首页，此处待修正
+          redirect_to user_path(user.id)
         end
       end
-
-    #********* end *********#
   end
 end
