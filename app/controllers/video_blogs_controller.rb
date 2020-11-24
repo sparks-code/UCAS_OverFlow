1f8f73b1d113632cc8fcc2c55778734f9e6ce8dc
zenghao <2817386828@qq.com>
2020-11-24 20:04:02 +0800

    add video play function(with bug)

--- app/controllers/video_blogs_controller.rb
+++ app/controllers/video_blogs_controller.rb
@@ -59,10 +59,10 @@ class VideoBlogsController < ApplicationController
 
 
     #设置目录路径，如果目录不存在，生成新目录
-    FileUtils.mkdir("#{Rails.root}/public/upload") unless File.exist?("#{Rails.root}/public/upload")
+    FileUtils.mkdir("#{Rails.root}/app/assets/videos") unless File.exist?("#{Rails.root}/app/assets/videos")
     #写入文件
     ##wb 表示通过二进制方式写，可以保证文件不损坏
-    File.open("#{Rails.root}/public/upload/#{@filename}", "wb") do |f|
+    File.open("#{Rails.root}/app/assets/videos/#{@filename}", "wb") do |f|
       f.write(file.read)
     end
     return @filename
