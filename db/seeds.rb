61f3488afc0afcae4736ad5aeb92740ec1cda3ef
oneflyingfish <yt931379339@gmail.com>
2020-12-03 21:50:06 -0800

    fix seed rules and users show bug

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,30 +9,18 @@
 require 'roo'
 
 # 初始化Users
-# 99.times do |n|
-#     name  = Faker::Name.name
-#     num = "%03d"%(n+1)
-#     user_number="#{rand(2019..2022)}#{rand(1..2)}#{rand(50149..50152)}#{rand(55..60)}#{num}"
-#     email = "test_#{n+1}@eamil.com"
-#     password = "password"
-#     sex = rand(1..2)==1 ? "男" : "女"
-#     activated=true
-#     User.create!(name: name ,user_number: user_number, email: email,sex: sex , password: password, password_confirmation: password,activated: activated)
-# end
+99.times do |n|
+    name  = Faker::Name.name
+    num = "%03d"%(n+1)
+    user_number="#{rand(2019..2022)}#{rand(1..2)}#{rand(80100..80200)}#{'%02d'%rand(1..60)}#{num}"
+    email = "test_#{n+1}@eamil.com"
+    password = "password"
+    sex = rand(1..2)==1 ? "男" : "女"
+    activated=true
+    User.create!(name: name ,user_number: user_number, email: email,sex: sex , password: password, password_confirmation: password,activated: activated)
+end
 
-
-# 初始化学院、研究所、专业
-# 99.times do |n|
-#     code_number_project = "#{"%02d"%(n)}"
-#     project_name= "#{n%20 + 50}专业"
-#     Project.create!(code_number: code_number_project, project_name: project_name)
-
-#     code_number_academy_organization = "#{80100+n}"
-
-#     organization_name = "#{n+1}研究所"
-#     academy_name = "#{n+675}学院"
-#     AcademyOrganization.create!(code_number: code_number_academy_organization, academy_name: academy_name, organization_name: organization_name)
-# end
+User.create!(name: "余甜" ,user_number: "202028015059033", email: "a_flying_fish@outlook.com",sex: "男" , password: "123", password_confirmation: "123",activated: true, admin: true)
 
 # 初始化学院
 xlsx = Roo::Excelx.new(File.expand_path('./db/school_info/academy_organization.xlsx'))
